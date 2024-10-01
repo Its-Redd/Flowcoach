99b0b7519a5059dc8b55af53bd95788a0fc440fa
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-10-01 11:13:41 +0200

    Create Blazor Project

--- src/FlowCoach.DataAccess/DataContext.cs
+++ src/FlowCoach.DataAccess/DataContext.cs
@@ -5,10 +5,29 @@ namespace FlowCoach.DataAccess
 {
     public class DataContext : DbContext
     {
+        private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FlowcoachDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
-        public DbSet<Card> Cards { get; set; }
+        public DbSet<EmotionCard> EmotionCards { get; set; }
+        public DbSet<FlowcoachCard> FlowcoachCards { get; set; }
+        public DbSet<SelfcareCard> SelfcareCards { get; set; }
+        public DbSet<BodyFlowCard> BodyFlowCards { get; set; }
+        public DbSet<BodyFlowArticle> BodyFlowArticles { get; set; }
+        public DbSet<SelfCareArticle> SelfcareArticles { get; set; }
+        public DbSet<CoachingSession> CoachingSessions { get; set; }
+        public DbSet<JournalEntry> JournalEntries { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+
 
     }
 }
