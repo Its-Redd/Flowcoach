2ed510e3633220f2af03f1e424cd361107e31ef9
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-10-03 09:46:01 +0200

    Possibly fixed broken infrastructure

---
+++ src/Flowcoach.Services/SignalRService.cs
@@ -0,0 +1,19 @@
+﻿namespace FlowCoach.App;
+using Microsoft.AspNetCore.Components;
+
+public class SignalRService : IAsyncDisposable
+{
+    public SignalRService(NavigationManager navigationManager)
+    {
+
+    }
+    public ValueTask DisposeAsync()
+    {
+        return default;
+    }
+
+
+}
+
+
+
