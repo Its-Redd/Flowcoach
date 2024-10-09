46450e31b0b032175fcea2e8032b5eada0d43fa0
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-10-09 14:46:56 +0200

    Implement Generic Repository

---
+++ src/FlowCoach.DataAccess/IRepository.cs
@@ -0,0 +1,47 @@
+﻿namespace FlowCoach.DataAccess
+{
+    /// <summary>
+    /// Represents a generic repository interface for CRUD operations.
+    /// </summary>
+    /// <typeparam name="T">The type of entity.</typeparam>
+    public interface IRepository<T>
+    {
+        /// <summary>
+        /// Adds a new entity to the repository.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        void Add(T entity);
+
+        /// <summary>
+        /// Updates an existing entity in the repository.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Deletes an entity from the repository.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        void Delete(T entity);
+
+        /// <summary>
+        /// Deletes an entity from the repository by its ID. * what the f... is this?
+        /// </summary>
+        /// <param name="id">The ID of the entity to delete.</param>
+        void Delete(int id);
+
+        /// <summary>
+        /// Gets all entities from the repository.
+        /// </summary>
+        /// <returns>An enumerable collection of entities.</returns>
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// Gets an entity from the repository by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the entity to get.</param>
+        /// <returns>The entity with the specified ID.</returns>
+        T GetBy(int id);
+    }
+
+}
---
+++ src/FlowCoach.DataAccess/Repository.cs
@@ -0,0 +1,35 @@
+﻿namespace FlowCoach.DataAccess
+{
+    internal class Repository<T> : IRepository<T>
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
