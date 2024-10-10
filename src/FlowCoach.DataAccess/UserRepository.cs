c97b1b8f60929d6625524b7e813ac280f81f6b5b
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-10-10 14:27:49 +0200

    Create UserController

--- src/FlowCoach.DataAccess/UserRepository.cs
+++ src/FlowCoach.DataAccess/UserRepository.cs
@@ -17,4 +17,4 @@ namespace FlowCoach.DataAccess
             return false;
         }
     }
-}
+}       
---
+++ src/Flowcoach.API/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using FlowCoach.DataAccess;
+using FlowCoach.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Flowcoach.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUserRepository repository) : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            if (repository.Login(user.Email, user.Password))
+            {
+                return Ok();
+            }
+            return Unauthorized();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] User user)
+        {
+            repository.Add(user);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(repository.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return Ok(repository.GetBy(id));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] User user)
+        {
+            repository.Update(user);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return Ok();
+        }
+    }
+}
