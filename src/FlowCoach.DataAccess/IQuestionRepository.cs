5d20c60d0ff8cc4013caf8b433ba661db2ad45c9
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-10-10 11:34:05 +0200

    Create Question Interface and repository

---
+++ src/FlowCoach.DataAccess/IQuestionRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlowCoach.Entities;
+
+namespace FlowCoach.DataAccess
+{
+    public interface IQuestionRepository : IRepository<Question>
+    {
+        public IEnumerable<Question> GetFirst5QuestionsWith(QuestionCategory questionCategory);
+
+        public IEnumerable<Question> GetNext5QuestionsWith(int lastQuestionId, QuestionCategory questionCategory);
+
+        public IEnumerable<Question> GetAllQuestionsWith(QuestionCategory questionCategory);
+    }
+}
---
+++ src/FlowCoach.DataAccess/QuestionRepository.cs
@@ -0,0 +1,26 @@
+﻿using FlowCoach.Entities;
+
+namespace FlowCoach.DataAccess
+{
+    public class QuestionRepository : Repository<Question>, IQuestionRepository
+    {
+        public QuestionRepository(DataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public IEnumerable<Question> GetAllQuestionsWith(QuestionCategory questionCategory)
+        {
+            return dataContext.Questions.Where(q => q.Category == questionCategory).ToList();
+        }
+
+        public IEnumerable<Question> GetFirst5QuestionsWith(QuestionCategory questionCategory)
+        {
+            return dataContext.Questions.Where(q => q.Category == questionCategory).Take(5).ToList();
+        }
+
+        public IEnumerable<Question> GetNext5QuestionsWith(int lastQuestionId, QuestionCategory questionCategory)
+        {
+            return dataContext.Questions.Where(q => q.Category == questionCategory && q.Id > lastQuestionId).Take(5).ToList();
+        }
+    }
+}
