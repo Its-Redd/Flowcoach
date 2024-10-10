3249e3b23442d32dc2346706a258a46ad01f0ec8
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-10-10 11:41:37 +0200

    AddScoped all repositories

--- src/Flowcoach.API/Program.cs
+++ src/Flowcoach.API/Program.cs
@@ -1,3 +1,7 @@
+using FlowCoach.DataAccess;
+using FlowCoach.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+builder.Services.AddScoped<IRepository<Card>, Repository<Card>>();
+builder.Services.AddScoped<IRepository<BodyFlowArticle>, Repository<BodyFlowArticle>>();
+builder.Services.AddScoped<IRepository<BodyFlowCard>, Repository<BodyFlowCard>>();
+builder.Services.AddScoped<IRepository<CoachingSession>, Repository<CoachingSession>>();
+builder.Services.AddScoped<IRepository<EmotionCard>, Repository<EmotionCard>>();
+builder.Services.AddScoped<IRepository<FlowcoachCard>, Repository<FlowcoachCard>>();
+builder.Services.AddScoped<IRepository<JournalEntry>, Repository<JournalEntry>>();
+builder.Services.AddScoped<IRepository<SelfcareCard>, Repository<SelfcareCard>>();
+builder.Services.AddScoped<IRepository<SelfCareArticle>, Repository<SelfCareArticle>>();
 
 var app = builder.Build();
 
